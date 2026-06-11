90bff8b0552f40b9af14dab0f7f23cef4bc32069
Thejec1 <jessecbrown91@gmail.com>
2026-06-11 06:15:48 +0000

    Testing

--- crm-main.py
+++ crm-main.py
@@ -1,3 +1,9 @@
+import json
+from pathlib import Path
+
+
 def main_menu():
     print("\n~~WELCOME TO CLERIUM~~")
-    print
+    print("Press 1 to add a new customer")
+    print("Press 2 to view customer")
+    print("Press 3 to exit")
